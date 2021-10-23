ef9e4fd680811835cbae5eaef3f2576c211e9c9e
leoda <leo.danilevich@mail.ru>
2021-10-23 09:22:28 +0300

    Hexagon is somehow generated

--- src/test.kt
+++ src/test.kt
@@ -1,11 +1,11 @@
 import generator.generateBoard
-import solver.hexagon.*
-import solver.rectangle.*
+import solver.Hexagon
+import solver.Rectangle
 
 fun main() {
     println("Do you want to solve any crossword or to create one?")
     print("s - solve, g - generate >")
-    when (readLine()!!.trim()) {
+    when ("g") {
         "s" -> solve()
         "g" -> generate()
     }
@@ -13,15 +13,15 @@ fun main() {
 
 fun generate(){
     print("r - rect, h - hex >")
-    val type = readLine()!!
-    if (type == "r") {
+    if (false) {
         TODO("Not implemented yet")
     } else {
         print("Enter crossword size: ")
-        val size = readLine()!!.toInt()
+        val size = 5
         val h = Hexagon(size)
         generateBoard(h, "ABCDEFGHRT".toList())
-        h.putToFile("src\\solver\\result.txt")
+        h.putToFile("src/solver/result.txt")
+        println()
         println(h)
     }
 }
@@ -33,14 +33,14 @@ fun solve() {
         print("Enter size (A)x(B) -> ")
         val (h, w) = readLine()!!.split(" ").map { it.toInt() }
         val fig = Rectangle(h, w)
-        fig.readFromFile("src\\solver\\sample_rect.txt")
+        fig.readFromFile("src/solver/sample_rect.txt")
         fig.solve()
         println(fig)
     } else if (type == "h") {
         print("Enter size -> ")
         val s = readLine()!!.toInt()
         val fig = Hexagon(s)
-        fig.readFromFile("src\\solver\\sample_hex.txt")
+        fig.readFromFile("src/solver/sample_hex.txt")
         fig.solve()
         println(fig)
     }
